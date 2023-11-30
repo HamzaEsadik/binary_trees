0d9437f087fcbfefc7c624b1c081d7105f03a0fb
Yassine Amgarou <90642482+Gnomedebian@users.noreply.github.com>
2023-11-30 16:02:33 +0100

    Update 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_leaf - check node leaf or not
- * @node: node to check
- * Return: 1 if node if leaf 0 if not
+ * binary_tree_is_leaf - check node leaf or not.
+ * @node: node to check.
+ * Return: 1 if node if leaf 0 if not.
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {

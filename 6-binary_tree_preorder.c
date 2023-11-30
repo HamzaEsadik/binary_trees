93c9a06d11aef4a27a4d7da9d7219a07c5a08ef7
Yassine Amgarou <90642482+Gnomedebian@users.noreply.github.com>
2023-11-30 16:01:28 +0100

    Update 6-binary_tree_preorder.c

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder - pre-order traversal
+ * binary_tree_preorder - pre-order traversal.
  *
- * @tree: pointer to the root node of the tree to traverse
+ * @tree: pointer to the root node of the tree to traverse.
  * @func: pointer to a function to call for each node
 */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))

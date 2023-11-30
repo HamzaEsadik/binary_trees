8578e0554bb5e51a8138711449357c8af5668bf5
HamzaEsadik <hamza.esadik.m@gmail.com>
2023-11-30 16:13:59 +0100

    task 10 and 11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -10,6 +10,7 @@
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	size_t size = 0;
+
 	if (tree == NULL)
 	{
 		return (0);
@@ -21,25 +22,4 @@ size_t binary_tree_size(const binary_tree_t *tree)
 		size++;
 	}
 	return (size);
-}
-
-int main(void)
-{
-    binary_tree_t *root;
-    size_t size;
-
-    root = binary_tree_node(NULL, 98);
-    root->left = binary_tree_node(root, 12);
-    root->right = binary_tree_node(root, 402);
-    binary_tree_insert_right(root->left, 54);
-    binary_tree_insert_right(root, 128);
-    binary_tree_print(root);
-
-    size = binary_tree_size(root);
-    printf("Size of %d: %lu\n", root->n, size);
-    size = binary_tree_size(root->right);
-    printf("Size of %d: %lu\n", root->right->n, size);
-    size = binary_tree_size(root->left->right);
-    printf("Size of %d: %lu\n", root->left->right->n, size);
-    return (0);
 }
